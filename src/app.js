6f64a7c069488c7e3025c798ff7505b12d3309bc
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-25 13:35:36 +0500

    update src/app.js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,16 @@
 import express from "express"
+import cors from "cors"
 
+// Initialize App
 const app = express()
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// Middlewares and Config
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
+app.use(express.static("public"))
+
+// Cors Config
+app.use(cors())
 
 app.get("/", (req, res) => {
   res.send("Hello World")

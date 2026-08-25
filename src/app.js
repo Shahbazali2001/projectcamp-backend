19499354db72971d6eaa24d3443abda9fa69a718
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-25 13:45:32 +0500

    added CORS config

--- src/app.js
+++ src/app.js
@@ -10,7 +10,14 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 
 // Cors Config
-app.use(cors())
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+)
 
 app.get("/", (req, res) => {
   res.send("Hello World")

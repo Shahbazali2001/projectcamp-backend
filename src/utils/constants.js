9b7acc61af2eab58f2a0c2e7414f4cdae02b4d29
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-27 10:41:57 +0500

    feat: add db and constants

---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+// User Roles
+export const userRolesEnum = {
+  ADMIN: "admin",
+  PROJECT_ADMIN: "project_admin",
+  MEMBER: "member",
+}
+export const AvailableUserRole = Object.values(userRolesEnum)
+
+
+
+
+// Task Status
+export const taskStatusEnum = {
+  TODO: "todo",
+  IN_PROGRESS: "in_progress",
+  DONE: "done",
+}
+export const AvailableTaskStatus = Object.values(taskStatusEnum)

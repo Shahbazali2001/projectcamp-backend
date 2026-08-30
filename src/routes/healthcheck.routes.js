8bf1cb9296d3840464840083f5d235eb622ac024
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-30 18:21:10 +0500

    fix: restore healthcheck and auth controllers and routes

--- src/routes/healthcheck.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express"
-import { healthCheck } from "../controllers/healthcheck.controllers.js"
-
-const healthCheckRouter = Router()
-// healthCheckRouter.get("/healthcheck", healthCheck)
-healthCheckRouter.route("/").get(healthCheck)
-
-export default healthCheckRouter

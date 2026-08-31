fa3e93b69729137c15d11a7878e525d16581cfbc
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-08-31 16:54:37 +0500

    fix: update app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors"
 
 // Routes Import
 import healthCheckRouter from "./routes/healthRoutes/healthcheck.routes.js"
+import authRouter from "./routes/authRoutes/auth.routes.js"
 
 // Initialize App
 const app = express()
@@ -12,6 +13,7 @@ app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/auth", authRouter)
 
 // Cors Config
 app.use(

a29b2d32e57b8013846d474bb763bc2df1f79e0c
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-09-01 09:57:33 +0500

    fix: correct validator middleware path

---
+++ src/middlewares/validator.middleware.js
@@ -0,0 +1,15 @@
+import { validationResult } from "express-validator"
+import { ApiError } from "../utils/api-error.js"
+import { th } from "zod/locales"
+
+export const validator = (req, res, next) => {
+  const errors = validationResult(req)
+  if (errors.isEmpty()) {
+    return next()
+  }
+
+  const extractedErrors = []
+  errors.array().map((err) => extractedErrors.push({ [err.path]: err.msg }))
+
+  throw new ApiError(400, "Validation Error", extractedErrors)
+}

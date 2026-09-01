89eb4765055482e4804585df92eb02224fd92a80
Shahbazali2001 <Shahbazali5097855@gmail.com>
2026-09-01 16:59:11 +0500

    update auth routes and validators

--- src/routes/authRoutes/auth.routes.js
+++ src/routes/authRoutes/auth.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import { registerUser } from "../../controllers/authControllers/registerUser.controller.js"
+import { userRegisterValidator } from "../../validators/index.js"
+import { validator } from "../../middlewares/validator.middleware.js"
 
 const authRouter = Router()
 
-authRouter.post("/register", registerUser)
+authRouter.post("/register", userRegisterValidator(), validator, registerUser)
 
 export default authRouter
